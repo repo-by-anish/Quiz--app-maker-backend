f52dc17f491e032ce0458a24aea5cc0da9866832
Dev-anish-kr <115490507+Dev-anish-kr@users.noreply.github.com>
2023-04-05 19:58:09 +0530

    Updated cors optionsd

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,4 +1,8 @@
 const allowedOrigins=[
-    "https://create-quiz.cyclic.app/"
+    "https://create-quiz.cyclic.app/",
+    "https://create-quiz.cyclic.app/quiz",
+    "https://create-quiz.cyclic.app/answere",
+    "http://localhost:3500",
+    ""
 ]
 module.exports=allowedOrigins;
