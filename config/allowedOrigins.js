9dbabe1e5f869c31110a2c427527fac55aa884cf
Dev-anish-kr <115490507+Dev-anish-kr@users.noreply.github.com>
2023-04-08 14:48:16 +0530

    Added Cors Option

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,12 +1,4 @@
 const allowedOrigins=[
-    "https://create-quiz.cyclic.app/",
-    "https://create-quiz.cyclic.app/*",
-    "https://create-quiz.cyclic.app/quiz",
-    "https://create-quiz.cyclic.app/answere",
-    "http://localhost:3500",
-    "",
-    "https://create-quiz.cyclic",
-    "/answere/*",
-    "/",
+    "https://quizcreate.netlify.app/",
 ]
 module.exports=allowedOrigins;
